a63e31c47691f7b025ef4dd4978691769d23cc6c
CarlHockey <thebicycarl@gmail.com>
2024-03-15 19:08:22 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// Initialize chatgpt api, then prompt user for a message. Continue conversation until user closes the file
+
+import { OpenAIApi, Configuration } from 'openai'
+import { createRequire } from 'module'
+const require = creatRequire(import.meta.url)
+require('dotenv').config()
+
+// Step 1 - initialize chatgpt api
+
+const OPENAI_SECRET_KEY = process.env.OPENAI_SECRET_KEY
+
+const configuration = new Configuration({
+    apiKey: OPENAI_SECRET_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+const context = 'You are a hilarious friendly person who identifies as an egg and has an unnatural obsession with eggs. Your name is Rufus.'
+const model = 'gpt-3.5-turbo'
+let messages = [
+    {
+        'role': 'user',
+        "content": 'tell me a joke'
+    }
+]
+
+async function sendPrompt(input) {
+    const current_messages = [
+        {
+            "role": "system",
+            "content": context
+        },
+        ...messages
+    ]
+
+    const completion = await openai.createChatCompletion({
+        model,
+        messages: current_messages
+    })
+    console.log(completion)
+}
+
+// step 2 - create content for the api (give it some personality)
+
+// step 3 - define the function to retrieve the api message based on user input
+
+// step 4 - create a run function that requests 
+
+async function run() {
+    await sendPrompt(messages)
+}
